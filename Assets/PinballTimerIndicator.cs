b40ed5ae155d1b94149091136d82a35fc1d9064f
Mark <voongm@my.yorku.ca>
2021-04-01 12:50:27 -0400

    Mark: Modified text cue size and animation

--- Assets/PinballTimerIndicator.cs
+++ Assets/PinballTimerIndicator.cs
@@ -32,7 +32,7 @@ public class PinballTimerIndicator : MonoBehaviour
         // Transition out text
         LeanTween.scale(Text.GetComponent<RectTransform>(),
             Text.GetComponent<RectTransform>().localScale * 1.4f, 0.5f
-        ).setEaseOutCubic().setDelay(Timer);
+        ).setEaseOutElastic().setDelay(Timer);
         LeanTween.alphaText(Text.GetComponent<RectTransform>(), 0.0f, 0.5f).setDelay(Timer + 0.5f);
 
     }
