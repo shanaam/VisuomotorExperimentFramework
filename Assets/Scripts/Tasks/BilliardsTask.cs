58618d806f1aa23c2fe6f180e2eeef2f2e378f26
shanaa <s.modcha@gmail.com>
2023-06-07 14:07:58 -0400

    fixed crashes on build due to scoreboard

--- Assets/Scripts/Tasks/BilliardsTask.cs
+++ Assets/Scripts/Tasks/BilliardsTask.cs
@@ -7,128 +7,128 @@ using System;
 
 public abstract class BilliardsTask : BaseTask
 {
-    protected ExperimentController ctrler;
+  protected ExperimentController ctrler;
 
-    public GameObject Surface;
-    protected Scoreboard scoreboard;
-    protected TimerIndicator timerIndicator;
+  public GameObject Surface;
+  protected Scoreboard scoreboard;
+  protected TimerIndicator timerIndicator;
 
-    protected bool trackScore;
+  protected bool trackScore;
 
-    protected float cameraTilt, surfaceTilt;
+  protected float cameraTilt, surfaceTilt;
 
-    // Minimum distance to score any points. this is also the cutoff distance
-    // for starting the miss timer
-    protected const float TARGET_DISTANCE = 1.0f; // Target distance from home
+  // Minimum distance to score any points. this is also the cutoff distance
+  // for starting the miss timer
+  protected const float TARGET_DISTANCE = 1.0f; // Target distance from home
 
-    public override void Setup()
+  public override void Setup()
+  {
+    Surface = GameObject.Find("Surface");
+    timerIndicator = GameObject.Find("TimerIndicator").GetComponent<TimerIndicator>();
+    scoreboard = GameObject.Find("Scoreboard").GetComponent<Scoreboard>();
+    ctrler = ExperimentController.Instance();
+
+    // Scoreboard is now updated by the billiards class
+    scoreboard.AllowManualSet = true;
+
+    cameraTilt = Convert.ToSingle(ctrler.PollPseudorandomList("per_block_list_camera_tilt"));
+    surfaceTilt = Convert.ToSingle(ctrler.PollPseudorandomList("per_block_list_surface_tilt"));
+    // cameraTilt -= surfaceTilt; // As surfaceTilt rotates the entire prefab, this line makes creating the json more intuitive 
+
+    // Whether or not this is a practice trial 
+    // replaces scoreboard with 'Practice Round', doesn't record score
+    trackScore = (ctrler.Session.CurrentBlock.settings.GetBool("per_block_track_score"));
+    if (!trackScore)
     {
-        Surface = GameObject.Find("Surface");
-        timerIndicator = GameObject.Find("TimerIndicator").GetComponent<TimerIndicator>();
-        scoreboard = GameObject.Find("Scoreboard").GetComponent<Scoreboard>();
-        ctrler = ExperimentController.Instance();
+      scoreboard.ScorePrefix = false;
+      scoreboard.ManualScoreText = "Practice Round";
+    }
+  }
 
-        // Scoreboard is now updated by the billiards class
-        scoreboard.AllowManualSet = true;
+  /// <summary>
+  /// Sets the target's position using targetAngle and TARGET_DISTANCE
+  /// </summary>
+  /// <param name="targetAngle">Angle to set the target (usually from JSON)</param>
+  protected virtual void SetTargetPosition(float targetAngle)
+  {
+    // initializes the position
+    Target.transform.position = new Vector3(0f, 0.065f, 0f);
+    //rotates the object
+    Target.transform.rotation = Quaternion.Euler(0f, -targetAngle + 90f, 0f);
+    //moves object forward towards the direction it is facing
+    Target.transform.position += Target.transform.forward.normalized * TARGET_DISTANCE;
+  }
 
-        cameraTilt = Convert.ToSingle(ctrler.PollPseudorandomList("per_block_list_camera_tilt"));
-        surfaceTilt = Convert.ToSingle(ctrler.PollPseudorandomList("per_block_list_surface_tilt"));
-        // cameraTilt -= surfaceTilt; // As surfaceTilt rotates the entire prefab, this line makes creating the json more intuitive 
 
-        // Whether or not this is a practice trial 
-        // replaces scoreboard with 'Practice Round', doesn't record score
-        trackScore = (ctrler.Session.CurrentBlock.settings.GetBool("per_block_track_score"));
-        if (!trackScore)
-        {
-            scoreboard.ScorePrefix = false;
-            scoreboard.ManualScoreText = "Practice Round";
-        }
+  /// <summary>
+  /// Rotates obj around axis by angle degrees
+  /// </summary>
+  /// <param name="obj">Object to be rotated</param>
+  /// <param name="point">Point to rotate around</param>
+  /// <param name="axis">Object to rotate around</param>
+  /// <param name="angle">Angle in degrees of rotation</param>
+  protected static void SetTilt(GameObject obj, Vector3 point, GameObject axis, float angle)
+  {
+    // Decouple object from parent
+    Transform parent = obj.transform.parent;
+    obj.transform.SetParent(null);
+
+    obj.transform.RotateAround(point, axis.transform.forward, angle);
+
+    // Reparent obj
+    obj.transform.SetParent(parent);
+  }
+
+  protected virtual void SetSurfaceMaterial(Material material)
+  {
+    if (Surface == null)
+    {
+      Debug.LogError("Surface was not found in the prefab. Please make sure it is added." +
+                     " If Surface exists in the prefab, check if you ran base.Setup()");
     }
 
-    /// <summary>
-    /// Sets the target's position using targetAngle and TARGET_DISTANCE
-    /// </summary>
-    /// <param name="targetAngle">Angle to set the target (usually from JSON)</param>
-    protected virtual void SetTargetPosition(float targetAngle)
+    if (material == null)
     {
-        // initializes the position
-        Target.transform.position = new Vector3(0f, 0.065f, 0f);
-        //rotates the object
-        Target.transform.rotation = Quaternion.Euler(0f, -targetAngle + 90f, 0f);
-        //moves object forward towards the direction it is facing
-        Target.transform.position += Target.transform.forward.normalized * TARGET_DISTANCE;
+      Debug.LogError("Material was not found. Check spelling.");
     }
 
-
-    /// <summary>
-    /// Rotates obj around axis by angle degrees
-    /// </summary>
-    /// <param name="obj">Object to be rotated</param>
-    /// <param name="point">Point to rotate around</param>
-    /// <param name="axis">Object to rotate around</param>
-    /// <param name="angle">Angle in degrees of rotation</param>
-    protected static void SetTilt(GameObject obj, Vector3 point, GameObject axis, float angle)
+    if (ctrler.Session.settings.GetString("experiment_mode") == "pinball_vr")
     {
-        // Decouple object from parent
-        Transform parent = obj.transform.parent;
-        obj.transform.SetParent(null);
-
-        obj.transform.RotateAround(point, axis.transform.forward, angle);
-        
-        // Reparent obj
-        obj.transform.SetParent(parent);
+      Surface.GetComponent<MeshRenderer>().material = material;
     }
 
-    protected virtual void SetSurfaceMaterial(Material material)
-    {
-        if (Surface == null)
-        {
-            Debug.LogError("Surface was not found in the prefab. Please make sure it is added." +
-                           " If Surface exists in the prefab, check if you ran base.Setup()");
-        }
+  }
 
-        if (material == null)
-        {
-            Debug.LogError("Material was not found. Check spelling.");
-        }
+  /// <summary>
+  /// Calculates tempScore based on distance to target.
+  /// </summary>
+  /// <returns>A float between 0-1 calculated linearly using currDistance, multiplied by MAX_POINTS.</returns>
+  /// <param name="currDistance">The current distance between the ball object and the target</param>
+  /// <param name="maxDistance">The furthest distance from the target that score should be received for (cannot be 0)</param>
+  protected virtual float CalculateScore(float currDistance, float maxDistance, float maxPoints)
+  {
+    float m = 1 / -maxDistance; // Slope of score calculation, so that currDistance = 0 -> max score, currDistance = maxDistance -> no score
 
-        if(ctrler.Session.settings.GetString("experiment_mode") == "pinball_vr") 
-        {
-            Surface.GetComponent<MeshRenderer>().material = material;
-        }
-        
-    }
+    return Mathf.Round(m * (currDistance - maxDistance) * maxPoints);
+  }
 
-    /// <summary>
-    /// Calculates tempScore based on distance to target.
-    /// </summary>
-    /// <returns>A float between 0-1 calculated linearly using currDistance, multiplied by MAX_POINTS.</returns>
-    /// <param name="currDistance">The current distance between the ball object and the target</param>
-    /// <param name="maxDistance">The furthest distance from the target that score should be received for (cannot be 0)</param>
-    protected virtual float CalculateScore(float currDistance, float maxDistance, float maxPoints)
-    {
-        float m = 1 / -maxDistance; // Slope of score calculation, so that currDistance = 0 -> max score, currDistance = maxDistance -> no score
+  /// <summary>
+  /// 
+  /// </summary>
+  protected virtual void GetFinalScore()
+  {
 
-        return Mathf.Round(m * (currDistance - maxDistance) * maxPoints);
-    }
+  }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    protected virtual void GetFinalScore()
-    {
-        
-    }
-
-    /// <summary>
-    /// Projects mouse onto the surface plane.
-    /// (Useful for when the surface plane is tilted)
-    /// </summary>
-    protected virtual Vector3 GetMousePoint(Transform ball)
-    {
-        return ctrler.CursorController.MouseToPlanePoint(
-                            Surface.transform.up * ball.position.y,
-                            ball.position,
-                            Camera.main);
-    }
+  /// <summary>
+  /// Projects mouse onto the surface plane.
+  /// (Useful for when the surface plane is tilted)
+  /// </summary>
+  protected virtual Vector3 GetMousePoint(Transform ball)
+  {
+    return ctrler.CursorController.MouseToPlanePoint(
+                        Surface.transform.up * ball.position.y,
+                        ball.position,
+                        Camera.main);
+  }
 }
